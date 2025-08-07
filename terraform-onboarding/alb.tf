cc0b738217ca1e50925a40448919c412e579608f
Mohamed Ben Kedim <mohamedbenkedim@gmail.com>
2025-08-07 14:14:08 +0100

    added HTTPS listener and SSL certificate to the new DNS name

--- terraform-onboarding/alb.tf
+++ terraform-onboarding/alb.tf
@@ -48,21 +48,27 @@ resource "aws_lb_listener" "onboarding_http" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"  # Permanent redirect
+    }
+  }
+}
+
+
+#HTTPS listener commented out for testing - no SSL certificate needed
+resource "aws_lb_listener" "onboarding_https" {
+  load_balancer_arn = aws_lb.onboarding.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate.onboarding.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.onboarding.arn
   }
 }
-
-# HTTPS listener commented out for testing - no SSL certificate needed
-# resource "aws_lb_listener" "onboarding_https" {
-#   load_balancer_arn = aws_lb.onboarding.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.onboarding.certificate_arn
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.onboarding.arn
-#   }
-# }
